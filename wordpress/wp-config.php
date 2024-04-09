fabfb3a821de255975fcefc91bdca620429f8c92
polianskii.ilia <polianskii.ilia@gmail.com>
2024-04-09 08:55:41 +0200

    jsxc img upgrade

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -3,7 +3,7 @@
  * The base configuration for WordPress
  *
  * The wp-config.php creation script uses this file during the installation.
- * You don't have to use the web site, you can copy this file to "wp-config.php"
+ * You don't have to use the website, you can copy this file to "wp-config.php"
  * and fill in the values.
  *
  * This file contains the following configurations:
@@ -32,7 +32,7 @@ define( 'DB_PASSWORD', 'root' );
 define( 'DB_HOST', 'mysql' );
 
 /** Database charset to use in creating database tables. */
-define( 'DB_CHARSET', 'utf8mb4' );
+define( 'DB_CHARSET', 'utf8' );
 
 /** The database collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
